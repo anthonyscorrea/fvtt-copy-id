c7a677b457624a2b84f53d2089f19fec01f6e443
Yannik <yannik.braendle@t-online.de>
2022-04-14 02:06:53 +0200

    added fallback if clipboard permission is missing

--- copyID.js
+++ copyID.js
@@ -1,4 +1,25 @@
+function copyIdText(toCopy) {
+    //check if directly writing to clipboard is possible ("unsecure" sources are blocked from clipboard)
+    if (navigator.clipboard != undefined) {
+        navigator.clipboard.writeText(toCopy);
+        console.log("CopyID | Copied the ID: " + toCopy);
+    }
+    else {
+        window.prompt("Copy to clipboard: Ctrl+C, Enter", toCopy);
+        console.log("CopyID | Prompt with ID due to lack of permission with: " + toCopy);
+        if (!game.settings.get("fvtt-copy-id", "suppressWarningPopups")) {
+            ui.notifications.warn("CopyID | Fallback due to lacking clipboard permission! (no SSL/HTTPS? --> check README)");
+            console.warn("CopyID | Fallback to prompt since clipboard permission is lacking. Check README for more information on how to fix.");
+        }
+    }
+};
+
 Hooks.on('ready', function () {
+    //Warn gm on lack of permissions
+    if (location.protocol !== 'https:' && game.user.isGM && !game.settings.get("fvtt-copy-id", "suppressWarningPopups")) {
+        ui.notifications.warn("CopyID | Without HTTPS/SSL the permission to write to clipboard is missing. Check Readme for more info.");
+        console.warn("CopyID | Without HTTPS/SSL the permission to write to clipboard is missing. Check Readme for more info.");
+    }
     if (!game.settings.get("fvtt-copy-id", "copyIdVisibility") || game.user.isGM) {
         //actor window
         Hooks.on('getActorSheetHeaderButtons', function (actor, buttons) {
@@ -8,20 +29,17 @@ Hooks.on('ready', function () {
                 class: "copy-ID",
                 icon: "fas fa-paperclip",
                 onclick: () => {
-                    navigator.clipboard.writeText("@Actor[" + actor.id.substring(6) + "]{" + actor.actor.name + "}");
-                    console.log("CopyID | Copied the ID: " + "@Actor[" + actor.id.substring(6) + "]{" + actor.actor.name + "}");
+                    copyIdText("@Actor[" + actor.id.substring(6) + "]{" + actor.actor.name + "}");
                 }
             };
             buttons.unshift(copyIDButton);
-
             return buttons;
         });
         //item window
         Hooks.on('renderItemSheet', (app, html, data) => {
             let openButton = $(`<a class="copy-ID" title="ID"><i class="fas fa-paperclip"></i>ID</a>`);
             openButton.click(event => {
-                navigator.clipboard.writeText("@Item[" + app.item.id + "]{" + app.item.name + "}");
-                console.log("CopyID | Copied the ID: " + "@Item[" + app.item.id + "]{" + app.item.name + "}");
+                copyIdText("@Item[" + app.item.id + "]{" + app.item.name + "}");
             });
 
             html.closest('.app').find('.copy-ID').remove();
@@ -32,8 +50,7 @@ Hooks.on('ready', function () {
         Hooks.on('renderSceneConfig', (app, html, data) => {
             let openButton = $(`<a class="copy-ID" title="ID"><i class="fas fa-paperclip"></i>ID</a>`);
             openButton.click(event => {
-                navigator.clipboard.writeText("@Scene[" + app.id.substring(13) + "]{" + app.object.name + "}");
-                console.log("CopyID | Copied the ID: " + "@Scene[" + app.id.substring(13) + "]{" + app.object.name + "}");
+                copyIdText("@Scene[" + app.id.substring(13) + "]{" + app.object.name + "}");
             });
             html.closest('.app').find('.copy-ID').remove();
             let titleElement = html.closest('.app').find('.header-button');
